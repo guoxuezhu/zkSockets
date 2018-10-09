006eecd1d4cce1ccf4a0eeb69b36f522c0713693
gxz <1174327885@qq.com>
2018-10-09 09:38:41 +0800

    fix ui

--- app/src/main/java/com/lh/zksockets/ui/VolumeSetActivity.java
+++ app/src/main/java/com/lh/zksockets/ui/VolumeSetActivity.java
@@ -3,19 +3,30 @@ package com.lh.zksockets.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.RadioButton;
 
 import com.lh.zksockets.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class VolumeSetActivity extends Activity {
+
+    @BindView(R.id.volume_radio_btn_1)
+    RadioButton volume_radio_btn_1;
+    @BindView(R.id.volume_radio_btn_2)
+    RadioButton volume_radio_btn_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volume_set);
         ButterKnife.bind(this);
+        volume_radio_btn_1.setChecked(true);
+        volume_radio_btn_2.setChecked(false);
+
+
 
     }
 
