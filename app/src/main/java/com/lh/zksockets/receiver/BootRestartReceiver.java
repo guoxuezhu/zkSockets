9e3196a430cbb61ea0b5cc64f54f9d422e473dac
guoxuezhu <1174327885@qq.com>
2019-04-15 09:29:35 +0800

    开机启动

---
+++ app/src/main/java/com/lh/zksockets/receiver/BootRestartReceiver.java
@@ -0,0 +1,24 @@
+package com.lh.zksockets.receiver;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.lh.zksockets.ui.LauncherActivity;
+
+
+public class BootRestartReceiver extends BroadcastReceiver {
+    private final String ACTION = "android.intent.action.BOOT_COMPLETED";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(ACTION)) {
+            Intent intent2 = new Intent(context, LauncherActivity.class);
+            intent2.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent2);
+           // ELog.i("开机自动服务自动启动...");
+        }
+
+    }
+}
