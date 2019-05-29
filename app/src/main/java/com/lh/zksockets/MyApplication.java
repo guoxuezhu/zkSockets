b1be1579a9db22b0bc0bacfaa24af9b99b17e523
gxz <1174327885@qq.com>
2019-05-29 09:37:29 +0800

    fix

--- app/src/main/java/com/lh/zksockets/MyApplication.java
+++ app/src/main/java/com/lh/zksockets/MyApplication.java
@@ -1,20 +1,24 @@
 package com.lh.zksockets;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.baidu.mobstat.StatService;
 import com.lh.zksockets.data.DbDao.DaoMaster;
 import com.lh.zksockets.data.DbDao.DaoSession;
+import com.lh.zksockets.utils.DateUtil;
 import com.lh.zksockets.utils.SharePreferenceUtil;
 
 public class MyApplication extends Application {
 
     public static SharePreferenceUtil prefs;
     public static DaoSession daoSession;
+    public static Context context;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        context = this;
 
         prefs = new SharePreferenceUtil(this, "zkcSaveDates");
 
@@ -22,9 +26,19 @@ public class MyApplication extends Application {
         DaoMaster daoMaster = new DaoMaster(devOpenHelper.getWritableDb());
         daoSession = daoMaster.newSession();
         StatService.start(this);
+
+        Thread.setDefaultUncaughtExceptionHandler(restartHandler); // 程序崩溃时触发线程  以下用来捕获程序崩溃异常
     }
 
     public static DaoSession getDaoSession() {
         return daoSession;
     }
+
+    // 创建服务用于捕获崩溃异常
+    private Thread.UncaughtExceptionHandler restartHandler = new Thread.UncaughtExceptionHandler() {
+        public void uncaughtException(Thread thread, Throwable ex) {
+//            String error = DateUtil.getNow() + " 发生崩溃异常时,重启应用 bug : " + ex.toString();
+            StatService.recordException(context, ex);
+        }
+    };
 }
