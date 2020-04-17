1305736813282ce82b8f2a9b5c982d740f8f12b9
gxz <1174327885@qq.com>
2020-04-17 16:24:56 +0800

    fix out

--- app/src/main/java/com/lh/zksockets/utils/SerialPortUtil.java
+++ app/src/main/java/com/lh/zksockets/utils/SerialPortUtil.java
@@ -553,14 +553,14 @@ public class SerialPortUtil {
         String msg = "";
         if (ml.substring(4).equals("1")) {
             msg = "{[ARM" + ml.substring(2, 3) + ":DT:A004]<OPEN>}";
-            if (dangerOutDao.load(Long.valueOf(ml.substring(2, 3))).dangerOutStatus == 0) {
-                TimerUtils.setHuifuDangerOutstatus(ml.substring(2, 3), dangerOutDao.load(Long.valueOf(ml.substring(2, 3))).time, 0);
-            }
+//            if (dangerOutDao.load(Long.valueOf(ml.substring(2, 3))).dangerOutStatus == 0) {
+//                TimerUtils.setHuifuDangerOutstatus(ml.substring(2, 3), dangerOutDao.load(Long.valueOf(ml.substring(2, 3))).time, 0);
+//            }
         } else if (ml.substring(4).equals("0")) {
             msg = "{[ARM" + ml.substring(2, 3) + ":DT:A005]<CLOSE>}";
-            if (dangerOutDao.load(Long.valueOf(ml.substring(2, 3))).dangerOutStatus == 1) {
-                TimerUtils.setHuifuDangerOutstatus(ml.substring(2, 3), dangerOutDao.load(Long.valueOf(ml.substring(2, 3))).time, 1);
-            }
+//            if (dangerOutDao.load(Long.valueOf(ml.substring(2, 3))).dangerOutStatus == 1) {
+//                TimerUtils.setHuifuDangerOutstatus(ml.substring(2, 3), dangerOutDao.load(Long.valueOf(ml.substring(2, 3))).time, 1);
+//            }
         }
         ELog.i("========doDanger====msg====" + msg);
         byte[] data = msg.getBytes();
@@ -578,14 +578,14 @@ public class SerialPortUtil {
         String msg = "";
         if (ml.substring(4).equals("1")) {
             msg = "{[IOL" + ml.substring(2, 3) + ":DT:A004]<OPEN>}";
-            if (ioPortDataDao.load(Long.valueOf(ml.substring(2, 3))).ioOutStatus == 0) {
-                TimerUtils.setHuifuIoOutstatus(ml.substring(2, 3), ioPortDataDao.load(Long.valueOf(ml.substring(2, 3))).time, 0);
-            }
+//            if (ioPortDataDao.load(Long.valueOf(ml.substring(2, 3))).ioOutStatus == 0) {
+//                TimerUtils.setHuifuIoOutstatus(ml.substring(2, 3), ioPortDataDao.load(Long.valueOf(ml.substring(2, 3))).time, 0);
+//            }
         } else if (ml.substring(4).equals("0")) {
             msg = "{[IOL" + ml.substring(2, 3) + ":DT:A005]<CLOSE>}";
-            if (ioPortDataDao.load(Long.valueOf(ml.substring(2, 3))).ioOutStatus == 1) {
-                TimerUtils.setHuifuIoOutstatus(ml.substring(2, 3), ioPortDataDao.load(Long.valueOf(ml.substring(2, 3))).time, 1);
-            }
+//            if (ioPortDataDao.load(Long.valueOf(ml.substring(2, 3))).ioOutStatus == 1) {
+//                TimerUtils.setHuifuIoOutstatus(ml.substring(2, 3), ioPortDataDao.load(Long.valueOf(ml.substring(2, 3))).time, 1);
+//            }
         }
         ELog.i("========doIO====msg====" + msg);
         byte[] data = msg.getBytes();
