a0b9f7a14dfe292d647afc20e41d9d159879f8bf
gxz <1174327885@qq.com>
2020-11-26 17:07:44 +0800

    fix UI

--- app/src/main/java/com/lh/zksockets/ui/PcEventActivity.java
+++ app/src/main/java/com/lh/zksockets/ui/PcEventActivity.java
@@ -25,6 +25,14 @@ public class PcEventActivity extends BaseActivity {
     EditText event_et_5003;
     @BindView(R.id.event_et_5004)
     EditText event_et_5004;
+    @BindView(R.id.event_et_5005)
+    EditText event_et_5005;
+    @BindView(R.id.event_et_5006)
+    EditText event_et_5006;
+    @BindView(R.id.event_et_5007)
+    EditText event_et_5007;
+    @BindView(R.id.event_et_5008)
+    EditText event_et_5008;
 
     @BindView(R.id.event_tv_time_5001)
     TextView event_tv_time_5001;
@@ -34,6 +42,15 @@ public class PcEventActivity extends BaseActivity {
     TextView event_tv_time_5003;
     @BindView(R.id.event_tv_time_5004)
     TextView event_tv_time_5004;
+    @BindView(R.id.event_tv_time_5005)
+    TextView event_tv_time_5005;
+    @BindView(R.id.event_tv_time_5006)
+    TextView event_tv_time_5006;
+    @BindView(R.id.event_tv_time_5007)
+    TextView event_tv_time_5007;
+    @BindView(R.id.event_tv_time_5008)
+    TextView event_tv_time_5008;
+
 
     private MLsListsDao mLsListsDao;
 
@@ -48,11 +65,19 @@ public class PcEventActivity extends BaseActivity {
         event_et_5002.setText(mLsListsDao.load((long) 5002).strMLs);
         event_et_5003.setText(mLsListsDao.load((long) 5003).strMLs);
         event_et_5004.setText(mLsListsDao.load((long) 5004).strMLs);
+        event_et_5005.setText(mLsListsDao.load((long) 5005).strMLs);
+        event_et_5006.setText(mLsListsDao.load((long) 5006).strMLs);
+        event_et_5007.setText(mLsListsDao.load((long) 5007).strMLs);
+        event_et_5008.setText(mLsListsDao.load((long) 5008).strMLs);
 
         event_tv_time_5001.setText(mLsListsDao.load((long) 5001).time);
         event_tv_time_5002.setText(mLsListsDao.load((long) 5002).time);
         event_tv_time_5003.setText(mLsListsDao.load((long) 5003).time);
         event_tv_time_5004.setText(mLsListsDao.load((long) 5004).time);
+        event_tv_time_5005.setText(mLsListsDao.load((long) 5005).time);
+        event_tv_time_5006.setText(mLsListsDao.load((long) 5006).time);
+        event_tv_time_5007.setText(mLsListsDao.load((long) 5007).time);
+        event_tv_time_5008.setText(mLsListsDao.load((long) 5008).time);
 
     }
 
@@ -64,6 +89,10 @@ public class PcEventActivity extends BaseActivity {
         mLsListsDao.update(new MLsLists((long) 5002, "大屏一体机关", event_et_5002.getText().toString(), event_tv_time_5002.getText().toString()));
         mLsListsDao.update(new MLsLists((long) 5003, "大屏一体机内置HDMI", event_et_5003.getText().toString(), event_tv_time_5003.getText().toString()));
         mLsListsDao.update(new MLsLists((long) 5004, "大屏一体机外置HDMI", event_et_5004.getText().toString(), event_tv_time_5004.getText().toString()));
+        mLsListsDao.update(new MLsLists((long) 5005, "电视机1", event_et_5005.getText().toString(), event_tv_time_5005.getText().toString()));
+        mLsListsDao.update(new MLsLists((long) 5006, "电视机2", event_et_5006.getText().toString(), event_tv_time_5006.getText().toString()));
+        mLsListsDao.update(new MLsLists((long) 5007, "电视机3", event_et_5007.getText().toString(), event_tv_time_5007.getText().toString()));
+        mLsListsDao.update(new MLsLists((long) 5008, "电视机4", event_et_5008.getText().toString(), event_tv_time_5008.getText().toString()));
 
         Toast.makeText(this, "保存成功", Toast.LENGTH_SHORT).show();
     }
