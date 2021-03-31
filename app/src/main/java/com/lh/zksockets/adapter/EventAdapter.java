64c65b247687af6e14a3e1b2e0a923d195038e2e
guoxuezhu <1174327885@qq.com>
2021-03-31 17:15:48 +0800

    fix ui

---
+++ app/src/main/java/com/lh/zksockets/adapter/EventAdapter.java
@@ -0,0 +1,104 @@
+package com.lh.zksockets.adapter;
+
+import android.content.Context;
+import android.text.Editable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.lh.zksockets.R;
+import com.lh.zksockets.data.model.IcCard;
+import com.lh.zksockets.data.model.MLsLists;
+import com.lh.zksockets.utils.ELog;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnFocusChange;
+import butterknife.OnTextChanged;
+
+
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHolder> {
+
+    private Context mContext;
+    private List<MLsLists> datas;
+    private CallBack mCallBack;
+
+    public EventAdapter(Context context, List<MLsLists> data, CallBack callBack) {
+        this.datas = data;
+        this.mContext = context;
+        this.mCallBack = callBack;
+    }
+
+    @NonNull
+    @Override
+    public EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.event_item_1, parent, false);
+        return new EventViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EventViewHolder holder, int position) {
+        MLsLists mLsLists = datas.get(position);
+        holder.setItem(mLsLists, position);
+        holder.event_id.setText(mLsLists.id + "");
+        holder.event_name.setText(mLsLists.name);
+        holder.event_time.setText(mLsLists.time);
+        holder.event_et_ml.setText(mLsLists.strMLs);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return datas == null ? 0 : datas.size();
+    }
+
+    public void setDatas(List<MLsLists> dfsEventdatas) {
+        datas = dfsEventdatas;
+//        notifyDataSetChanged();
+    }
+
+    public interface CallBack {
+        void onSetingMl(int mPosition, String etml);
+    }
+
+    public class EventViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.event_id)
+        TextView event_id;
+        @BindView(R.id.event_name)
+        TextView event_name;
+        @BindView(R.id.event_time)
+        TextView event_time;
+        @BindView(R.id.event_et_ml)
+        EditText event_et_ml;
+
+        private MLsLists item;
+        private int mPosition;
+
+        public EventViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void setItem(MLsLists mLsLists, int position) {
+            item = mLsLists;
+            mPosition = position;
+        }
+
+        @OnTextChanged(value = R.id.event_et_ml, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
+        public void mlEtChanged(Editable editable) {
+            if (!editable.toString().equals(item.strMLs)) {
+                mCallBack.onSetingMl(mPosition, editable.toString());
+                ELog.i("==============OnTextChanged=====1111=======" + editable.toString());
+            }
+        }
+    }
+}
--- app/src/main/java/com/lh/zksockets/ui/DianfsEventActivity.java
+++ app/src/main/java/com/lh/zksockets/ui/DianfsEventActivity.java
@@ -6,37 +6,35 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.lh.zksockets.MyApplication;
 import com.lh.zksockets.R;
+import com.lh.zksockets.adapter.EventAdapter;
+import com.lh.zksockets.adapter.IcCardAdapter;
+import com.lh.zksockets.data.DbDao.IcCardDao;
 import com.lh.zksockets.data.DbDao.MLsListsDao;
+import com.lh.zksockets.data.model.IcCard;
 import com.lh.zksockets.data.model.MLsLists;
+import com.lh.zksockets.utils.AddCardDialog;
+import com.lh.zksockets.utils.DeleteDialog;
+import com.lh.zksockets.utils.ELog;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class DianfsEventActivity extends BaseActivity {
+public class DianfsEventActivity extends BaseActivity implements EventAdapter.CallBack {
 
-    @BindView(R.id.event_tv_time_8001)
-    TextView event_tv_time_8001;
-    @BindView(R.id.event_tv_time_8002)
-    TextView event_tv_time_8002;
-    @BindView(R.id.event_tv_time_8003)
-    TextView event_tv_time_8003;
-    @BindView(R.id.event_tv_time_8004)
-    TextView event_tv_time_8004;
-
-    @BindView(R.id.event_et_8001)
-    EditText event_et_8001;
-    @BindView(R.id.event_et_8002)
-    EditText event_et_8002;
-    @BindView(R.id.event_et_8003)
-    EditText event_et_8003;
-    @BindView(R.id.event_et_8004)
-    EditText event_et_8004;
-
+    @BindView(R.id.event_recyclerView_1)
+    RecyclerView event_recyclerView_1;
 
     private MLsListsDao mLsListsDao;
+    private EventAdapter mEventAdapter;
+    private List<MLsLists> dfsEventdatas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,26 +43,39 @@ public class DianfsEventActivity extends BaseActivity {
         ButterKnife.bind(this);
         mLsListsDao = MyApplication.getDaoSession().getMLsListsDao();
 
-        event_et_8001.setText(mLsListsDao.load((long) 8001).strMLs);
-        event_et_8002.setText(mLsListsDao.load((long) 8002).strMLs);
-        event_et_8003.setText(mLsListsDao.load((long) 8003).strMLs);
-        event_et_8004.setText(mLsListsDao.load((long) 8004).strMLs);
-
-        event_tv_time_8001.setText(mLsListsDao.load((long) 8001).time);
-        event_tv_time_8002.setText(mLsListsDao.load((long) 8002).time);
-        event_tv_time_8003.setText(mLsListsDao.load((long) 8003).time);
-        event_tv_time_8004.setText(mLsListsDao.load((long) 8004).time);
+        initEventRcyclerView();
 
     }
 
+    private void initEventRcyclerView() {
+        event_recyclerView_1.setLayoutManager(new LinearLayoutManager(this));
+        dfsEventdatas = mLsListsDao.queryBuilder()
+                .where(MLsListsDao.Properties.Name.like("%电风扇%"))
+                .orderAsc(MLsListsDao.Properties.Id)
+                .list();
+        mEventAdapter = new EventAdapter(this, dfsEventdatas, this);
+        event_recyclerView_1.setAdapter(mEventAdapter);
+        ELog.i("===========dfsEventdatas===========" + dfsEventdatas.toString());
+    }
+
+    @Override
+    public void onSetingMl(int mPosition, String etml) {
+        ELog.i("=========onSetingMl===11=====" + mPosition);
+        ELog.i("=========onSetingMl====22====" + etml);
+        dfsEventdatas.get(mPosition).setStrMLs(etml);
+        mEventAdapter.setDatas(dfsEventdatas);
+    }
 
     @OnClick(R.id.btn_event_dianfs_ok)
     public void btn_event_dianfs_ok() {
-        mLsListsDao.update(new MLsLists((long) 8001, "电风扇全关", event_et_8001.getText().toString(), event_tv_time_8001.getText().toString()));
-        mLsListsDao.update(new MLsLists((long) 8002, "电风扇全1级", event_et_8002.getText().toString(), event_tv_time_8002.getText().toString()));
-        mLsListsDao.update(new MLsLists((long) 8003, "电风扇全2级", event_et_8003.getText().toString(), event_tv_time_8003.getText().toString()));
-        mLsListsDao.update(new MLsLists((long) 8004, "电风扇全3级", event_et_8004.getText().toString(), event_tv_time_8004.getText().toString()));
-
+//        mLsListsDao.update(new MLsLists((long) 8001, "电风扇全关", event_et_8001.getText().toString(), event_tv_time_8001.getText().toString()));
+//        mLsListsDao.update(new MLsLists((long) 8002, "电风扇全1级", event_et_8002.getText().toString(), event_tv_time_8002.getText().toString()));
+//        mLsListsDao.update(new MLsLists((long) 8003, "电风扇全2级", event_et_8003.getText().toString(), event_tv_time_8003.getText().toString()));
+//        mLsListsDao.update(new MLsLists((long) 8004, "电风扇全3级", event_et_8004.getText().toString(), event_tv_time_8004.getText().toString()));
+        ELog.i("===========dfsEventdatas===========" + dfsEventdatas.toString());
+        for (int i = 0; i < dfsEventdatas.size(); i++) {
+            mLsListsDao.update(dfsEventdatas.get(i));
+        }
         Toast.makeText(this, "保存成功", Toast.LENGTH_SHORT).show();
     }
 
