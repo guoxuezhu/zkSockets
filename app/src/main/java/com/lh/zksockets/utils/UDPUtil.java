a1d4cf9f3a52e37dccc21e77fd1d5375a69e9a4c
gxz <1174327885@qq.com>
2021-05-22 15:49:28 +0800

    fix UI

--- app/src/main/java/com/lh/zksockets/utils/UDPUtil.java
+++ app/src/main/java/com/lh/zksockets/utils/UDPUtil.java
@@ -4,13 +4,22 @@ import com.lh.zksockets.MyApplication;
 import com.lh.zksockets.data.DbDao.WuangguanInfoDao;
 import com.lh.zksockets.data.model.WuangguanInfo;
 
-import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
-import java.net.SocketException;
 
 public class UDPUtil {
+
+    private static DatagramSocket udpSocket;
+
+    private static void init() {
+        try {
+            udpSocket = new DatagramSocket(10101);
+        } catch (Exception e) {
+            udpSocket = null;
+            e.printStackTrace();
+        }
+    }
 
     public static void doWangguan(String ml) {
         WuangguanInfoDao wangguandata = MyApplication.getDaoSession().getWuangguanInfoDao();
@@ -35,25 +44,48 @@ public class UDPUtil {
     }
 
     private static void sendUdpMsg(String ip, byte[] msgbyte) {
-        DatagramSocket cUdpSocket = null;
+        if (udpSocket == null) {
+            init();
+        }
         try {
-            cUdpSocket = new DatagramSocket(10101);
             DatagramPacket dp = new DatagramPacket(msgbyte, msgbyte.length);
             dp.setSocketAddress(new InetSocketAddress(ip, 10101));
-            cUdpSocket.send(dp);//发送一条广播信息
-        } catch (SocketException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            udpSocket.send(dp);//发送一条广播信息
         } catch (Exception e) {
+            udpSocket = null;
             e.printStackTrace();
-        } finally {
-            if (cUdpSocket != null) {
-                cUdpSocket.close();
-            }
         }
-
     }
 
+    public static void startReadUdpMsg() {
+        if (udpSocket == null) {
+            init();
+        }
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                while (true) {
+                    byte[] receBuf = new byte[1024];
+                    try {
+                        DatagramPacket recePacket = new DatagramPacket(receBuf, receBuf.length);
+                        udpSocket.receive(recePacket);
+                        String readStr = new String(recePacket.getData(), 0, recePacket.getLength());
+                        ELog.d("=====接收数据包=======" + readStr);
+                        ELog.d("=====接收数据==服务端ip======" + recePacket.getAddress());
+//                        WuangguanInfoDao wangguandata = MyApplication.getDaoSession().getWuangguanInfoDao();
+//                        List<WuangguanInfo> wgData = wangguandata.queryBuilder()
+//                                .where(WuangguanInfoDao.Properties.Wg_ip.eq(recePacket.getAddress()),
+//                                        WuangguanInfoDao.Properties.Wg_status.eq(1))
+//                                .list();
+//                        if (wgData.size() != 0) {
+//                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
+    }
 
 }
